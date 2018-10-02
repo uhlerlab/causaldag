e4b09dc44be7d880ad7dd8976bde03b10645fc1c
csquires <chandlersquires18@gmail.com>
2018-10-02 19:47:19 -0400

    optimize random

--- causaldag/rand/graphs.py
+++ causaldag/rand/graphs.py
@@ -1,6 +1,7 @@
 import numpy as np
 from ..classes.dag import DAG
 import itertools as itr
+from more_itertools import chunked
 
 
 def _coin(p, size=1):
@@ -13,7 +14,11 @@ def directed_erdos(n, s, size=1):
         arcs = {(i, j) for (i, j), b in zip(itr.combinations(range(n), 2), bools) if b}
         return DAG(nodes=set(range(n)), arcs=arcs)
     else:
-        return [directed_erdos(n, s) for _ in range(size)]
+        all_bools = _coin(s, size=int(n*(n-1)/2)*size)
+        chunked_bools = chunked(all_bools, size)
+        possible_arcs = list(itr.combinations(range(n), 2))
+        chunked_arcs = [{(i, j) for (i, j), b in zip(possible_arcs, bools)} for bools in chunked_bools]
+        return [DAG(nodes=set(range(n)), arcs=arcs) for arcs in chunked_arcs]
 
 
 __all__ = ['directed_erdos']
