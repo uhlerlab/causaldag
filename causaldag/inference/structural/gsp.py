0a338d0a68bbb23f9c7b1bad99d40a02b72e7455
csquires <chandlersquires18@gmail.com>
2019-01-07 19:42:41 -0500

    finish first draft of UTIGSP implementation

--- causaldag/inference/structural/gsp.py
+++ causaldag/inference/structural/gsp.py
@@ -35,7 +35,6 @@ def _reverse_arc_gsp(dag, covered_arcs, i, j, suffstat, ci_test, alpha):
     # only change what comes before i and j so only effect set of arcs going into i and j
     # anything that's not a parent can't become a parent
     new_dag = dag.copy()
-    new_covered_arcs = covered_arcs.copy()
     parents = dag.parents_of(i)
 
     new_dag.reverse_arc(i, j)
@@ -50,12 +49,11 @@ def _reverse_arc_gsp(dag, covered_arcs, i, j, suffstat, ci_test, alpha):
             if p_j > alpha:
                 new_dag.remove_arc(parent, j)
 
-    for k, l in covered_arcs:
-        if k == i or l == i or k == j or l == j:
-            if not new_dag.has_arc(k, l):
-                new_covered_arcs.remove((k, l))
-            elif new_dag.parents_of(k) != new_dag.parents_of(l) - {k}:
-                new_covered_arcs.remove((k, l))
+    # TODO: I DON'T THINK THIS IS MUCH FASTER THAN JUST RECOMPUTING THE COVERED ARCS
+    new_covered_arcs = covered_arcs.copy() - dag.incident_arcs(i) - dag.incident_arcs(j)
+    for k, l in new_dag.incident_arcs(i) | new_dag.incident_arcs(j):
+        if new_dag.parents_of(k) == new_dag.parents_of(l) - {k}:
+            new_covered_arcs.add((k, l))
 
     return new_dag, new_covered_arcs
 
@@ -74,10 +72,12 @@ def gsp(
     DAG.
 
     :param suffstat:
-    :param starting_perm: Permutation with which to begin the search algorithm.
+    :param nnodes:
     :param ci_test: Conditional independence test.
     :param alpha: Significance level for independence tests.
     :param depth: Maximum depth in depth-first search. Use None for infinite search depth.
+    :param nruns: Number of runs of the algorithm. Each run starts at a random permutation and the sparsest DAG from all
+    runs is returned.
     :param verbose:
     :return:
     """
@@ -111,6 +111,12 @@ def gsp(
                 else:
                     trace.append((current_dag, current_covered_arcs, next_dags))
                     current_dag, current_covered_arcs = next_dags.pop()
+
+                # covered_arcs_check = current_dag.reversible_arcs()
+                # if current_covered_arcs != covered_arcs_check:
+                #     print(current_covered_arcs, covered_arcs_check)
+                #     raise Exception
+
                 next_dags = [
                     _reverse_arc_gsp(current_dag, current_covered_arcs, i, j, suffstat, ci_test, alpha=alpha)
                     for i, j in current_covered_arcs
@@ -172,9 +178,10 @@ def is_icovered(
 
 def unknown_target_igsp(
         samples: Dict[FrozenSet, np.ndarray],
-        starting_perm: list,
+        suffstat: Any,
+        nnodes: int,
         ci_test: CI_Test,
-        invariance_ci_test: CI_Test,
+        invariance_test: InvarianceTest,
         alpha: float=0.05,
         alpha_invariance: float=0.05,
         depth: Optional[int]=4,
@@ -186,53 +193,139 @@ def unknown_target_igsp(
     DAG.
 
     :param samples:
-    :param starting_perm: Permutation with which to begin the algorithm.
+    :param suffstat:
     :param ci_test: Conditional independence test
-    :param invariance_ci_test: Conditional independence test used for determining
+    :param invariance_test: Conditional independence test used for determining
     invariance of conditional distributions to interventions.
-    :param alpha: Significance level
+    :param alpha: Significance level for conditional independence test
+    :param alpha_invariance: Significance level for invariance test
     :param depth: Maximum depth of search. Use None for infinite search depth.
+    :param nruns: Number of runs of the algorithm. Each run starts at a random permutation and the lowest-scoring
+    DAG from all runs is returned.
     :param verbose:
     :return:
     """
-    # === STARTING VALUES
-    current_dag = perm2dag(samples[frozenset()], starting_perm, ci_test, alpha=alpha)
-    if verbose: print("=== STARTING DAG:", current_dag)
-    current_covered_arcs = current_dag.reversible_arcs()
-    current_i_covered_arcs = [
-        (i, j) for i, j in current_covered_arcs
-        if is_icovered(samples, i, j, current_dag, invariance_ci_test, alpha=alpha_invariance)]
+    # === DICTIONARY CACHING RESULTS OF ALL INVARIANCE TESTS SO FAR
+    is_variant_dict = {iv_nodes: dict() for iv_nodes in samples if iv_nodes != frozenset}
+    obs_samples = samples[frozenset()]
 
-    if verbose: print("=== STARTING I-COVERED ARCS:", current_i_covered_arcs)
+    # === HELPER METHODS
+    def _get_is_variant(iv_nodes, j, cond_set):
+        """
+        Check if in the intervention on iv_nodes, the conditional distribution of j given cond_set is the same as
+        the observational distribution. Cache through is_variant_dict.
+        """
+        is_variant = is_variant_dict[iv_nodes].get((j, cond_set))
+        if is_variant is None:
+            is_variant = invariance_test(
+                obs_samples, samples[iv_nodes], j, cond_set=list(cond_set), alpha=alpha_invariance)['reject']
+            is_variant_dict[iv_nodes][(j, cond_set)] = is_variant
+        return is_variant
 
-    # === RECORDS FOR DEPTH-FIRST SEARCH
-    all_visited_dags = set()
-    trace = []
+    def _is_icovered(i, j, dag):
+        """
+        Check if the edge i->j is I-covered in the DAG dag
+        """
+        parents_j = frozenset(dag.parents_of(j))
+        is_icov = True
+        for iv_nodes, iv_samples in samples.items():
+            if i in iv_nodes:
+                if not _get_is_variant(iv_nodes, j, parents_j):
+                    is_icov = False
+        return is_icov
 
-    # === SEARCH!
-    while True:
-        all_visited_dags.add(frozenset(current_dag.arcs))
+    def _get_num_variant(dag):
+        """
+        Count the number of variances for the DAG dag
+        """
+        num_variant = 0
+        for i in dag.nodes:
+            parents_i = frozenset(dag.parents_of(i))
+            for iv_nodes, iv_samples in samples.items():
+                if iv_nodes != frozenset():
+                    if _get_is_variant(iv_nodes, i, parents_i): num_variant += 1
 
-        # HERE'S THE TRICKY PART - THIS WON'T WORK OUTSIDE THE MARKOV EQUIVALENCE CLASS
-        next_dags = [current_dag.copy().reverse_arc(i, j, ignore_error=True) for i, j in current_i_covered_arcs]
-        lower_dags = None
+        return num_variant
 
-        if len(lower_dags) > 0:  # restart at lowest DAG
-            all_visited_dags = set()
-            trace = []
-            current_dag, current_i_covered_arcs = lower_dags[0]
-            if verbose: print("FOUND DAG WITH LOWER SCORE")
-        elif len(next_dags) > 0 and len(trace) != depth:
-            trace.append((current_dag, current_i_covered_arcs))
-            current_dag = next_dags[0]
-        # === DEAD END ===
-        elif len(trace) == 0:  # reached minimum within search depth
-            break
-        else:  # backtrack
-            current_dag, current_i_covered_arcs = trace.pop()
+    def _reverse_arc_igsp(dag, i_covered_arcs, i, j):
+        """
+        Return the DAG that comes from reversing the arc i->j, as well as its I-covered arcs and its score
+        """
+        new_dag = dag.copy()
+        parents = dag.parents_of(i)
 
-    return current_dag
+        new_dag.reverse_arc(i, j)
+        if parents:
+            for parent in parents:
+                rest = parents - {parent}
+                p_i = ci_test(suffstat, i, parent, [*rest, j], alpha=alpha)['p_value']
+                if p_i > alpha:
+                    new_dag.remove_arc(parent, i)
 
+                p_j = ci_test(suffstat, j, parent, cond_set=[*rest] if len(rest) != 0 else None, alpha=alpha)['p_value']
+                if p_j > alpha:
+                    new_dag.remove_arc(parent, j)
+
+        # new_i_covered_arcs = i_covered_arcs.copy() - dag.incident_arcs(i) - dag.incident_arcs(j)
+        # for k, l in new_dag.incident_arcs(i) | new_dag.incident_arcs(j):
+        #     if new_dag.parents_of(k) == new_dag.parents_of(l) - {k} and _is_icovered(i, j, dag):
+        #         new_i_covered_arcs.add((k, l))
+
+        new_covered_arcs = new_dag.reversible_arcs()
+        new_i_covered_arcs = [(i, j) for i, j in new_covered_arcs if _is_icovered(i, j, current_dag)]
+        new_score = len(new_dag.arcs) + _get_num_variant(dag)
+
+        return new_dag, new_i_covered_arcs, new_score
+
+    # === MINIMUM DAG AND SCORE FOUND BY ANY RUN
+    min_dag = None
+    min_score = float('inf')
+
+    # === MULTIPLE RUNS
+    for r in range(nruns):
+        # === STARTING VALUES
+        starting_perm = random.sample(list(range(nnodes)), nnodes)
+        current_dag = perm2dag(samples[frozenset()], starting_perm, ci_test, alpha=alpha)
+        current_score = len(current_dag.arcs) + _get_num_variant(current_dag)
+        if verbose: print("=== STARTING DAG:", current_dag, "== SCORE:", current_score)
+
+        current_covered_arcs = current_dag.reversible_arcs()
+        current_i_covered_arcs = [(i, j) for i, j in current_covered_arcs if _is_icovered(i, j, current_dag)]
+        if verbose: print("=== STARTING I-COVERED ARCS:", current_i_covered_arcs)
+        next_dags = [_reverse_arc_igsp(current_dag, current_i_covered_arcs, i, j)for i, j in current_i_covered_arcs]
+
+        # === RECORDS FOR DEPTH-FIRST SEARCH
+        all_visited_dags = set()
+        trace = []
+
+        # === SEARCH!
+        while True:
+            all_visited_dags.add(frozenset(current_dag.arcs))
+            lower_dags = [(d, i_cov_arcs, score) for d, i_cov_arcs, score in next_dags if score < current_score]
+
+            if (len(next_dags) > 0 and len(trace) != depth) or len(lower_dags) > 0:
+                if len(lower_dags) > 0:  # restart at a lower DAG
+                    all_visited_dags = set()
+                    trace = []
+                    current_dag, current_i_covered_arcs, current_score = lower_dags.pop()
+                    if verbose: print("FOUND DAG WITH LOWER SCORE:", current_dag, "== SCORE:", current_score)
+                else:
+                    trace.append((current_dag, current_i_covered_arcs, next_dags))
+                    current_dag = next_dags.pop()
+                next_dags = [_reverse_arc_igsp(current_dag, current_i_covered_arcs, i, j)for i, j in current_i_covered_arcs]
+                next_dags = [(d, i_cov_arcs) for d, i_cov_arcs in next_dags if frozenset(d.arcs) not in all_visited_dags]
+            # === DEAD END ===
+            else:
+                if len(trace) == 0:  # reached minimum within search depth
+                    break
+                else:  # backtrack
+                    current_dag, current_i_covered_arcs, next_dags = trace.pop()
+
+        if min_dag is None or current_score < min_score:
+            min_dag = current_dag
+            min_score = current_score
+
+    return min_dag
 
 
 
--- tests/test_gsp.py
+++ tests/test_gsp.py
@@ -4,32 +4,34 @@ import numpy as np
 import causaldag as cd
 from causaldag.inference.structural import gsp, perm2dag, is_icovered
 from causaldag.utils.ci_tests import kci_test_vector, ki_test_vector, gauss_ci_test, kci_invariance_test
+import random
 
 
 class TestDAG(TestCase):
     # def test_gsp(self):
     #     ndags = 100
     #     nnodes = 8
-    #     nsamples = 1000
+    #     nsamples = 500
     #     nneighbors_list = list(range(1, 8))
     #
     #     mean_shds = []
     #     percent_consistent = []
     #     for nneighbors in nneighbors_list:
+    #         print('=== nneighbors = %s ===' % nneighbors)
+    #         print("generating DAGs")
     #         dags = cd.rand.directed_erdos(nnodes, nneighbors/(nnodes-1), ndags)
-    #         print("generated DAGs")
+    #         print("generating weights")
     #         gdags = [cd.rand.rand_weights(dag) for dag in dags]
-    #         print("generated weights")
+    #         print("generating samples")
     #         samples_by_dag = [gdag.sample(nsamples) for gdag in gdags]
-    #         print("generated samples")
+    #         print("computing correlation matrices")
     #         corr_by_dag = [np.corrcoef(samples, rowvar=False) for samples in samples_by_dag]
-    #         print("computed correlation matrices")
+    #         print("running GSP")
     #         est_dags = [
     #             gsp(dict(C=corr, n=nsamples), nnodes, gauss_ci_test, depth=4, nruns=10, alpha=.01)
     #             for corr in corr_by_dag
     #         ]
     #         # print([str(d) for d in est_dags])
-    #         print("ran GSP")
     #         shd_by_dag = np.array([est_dag.shd_skeleton(dag) for est_dag, dag in zip(est_dags, dags)])
     #         match_by_dag = np.sum(shd_by_dag == 0)
     #         mean_shds.append(shd_by_dag.mean())
@@ -37,32 +39,69 @@ class TestDAG(TestCase):
     #     print("Mean SHDs:", mean_shds)
     #     print("Percent consistent:", percent_consistent)
 
+    def test_utigsp(self):
+        ndags = 100
+        nnodes = 8
+        nsamples = 500
+        nneighbors_list = list(range(1, 8))
+
+        mean_shds = []
+        percent_consistent = []
+        for nneighbors in nneighbors_list:
+            print('=== nneighbors = %s ===' % nneighbors)
+            print("generating DAGs")
+            dags = cd.rand.directed_erdos(nnodes, nneighbors / (nnodes - 1), ndags)
+            print("generating weights")
+            gdags = [cd.rand.rand_weights(dag) for dag in dags]
+            print("generating samples")
+            samples_by_dag = []
+            for gdag in gdags:
+                iv_nodes = frozenset({random.randint(0, nnodes)})
+                samples_by_dag.append({
+                    frozenset(): gdag.sample(nsamples),
+                    iv_nodes: gdag.sample_interventional({iv_nodes: cd.GaussIntervention(0, 2)})
+                })
+            print("computing correlation matrices")
+            corr_by_dag = [np.corrcoef(samples[frozenset()], rowvar=False) for samples in samples_by_dag]
+            print("running GSP")
+            est_dags = [
+                gsp(dict(C=corr, n=nsamples), nnodes, gauss_ci_test, depth=4, nruns=10, alpha=.01)
+                for corr in corr_by_dag
+            ]
+            # print([str(d) for d in est_dags])
+            shd_by_dag = np.array([est_dag.shd_skeleton(dag) for est_dag, dag in zip(est_dags, dags)])
+            match_by_dag = np.sum(shd_by_dag == 0)
+            mean_shds.append(shd_by_dag.mean())
+            percent_consistent.append(match_by_dag / ndags)
+        print("Mean SHDs:", mean_shds)
+        print("Percent consistent:", percent_consistent)
+
     # def test_perm2dag(self):
     #     gdag = cd.GaussDAG([0, 1, 2], arcs={(0, 1): 5, (0, 2): 5})
     #     samples = gdag.sample(100)
     #     est_dag = perm2dag(samples, [2, 0, 1], kci_test, ki_test)
     #     print(est_dag)
     #
-    def test_isicovered(self):
-        num_trials = 100
-        alpha = .05
-        num_samples = 100
-
-        false_negatives = 0
-        for i in range(num_trials):
-            d = cd.GaussDAG([0, 1], arcs={(0, 1)})
-
-            samples = {}
-            samples[frozenset()] = d.sample(num_samples)
-            samples[frozenset({1})] = d.sample_interventional({1: cd.GaussIntervention(2, 1)}, num_samples)
-
-            d_rev = cd.DAG(arcs={(1, 0)})
-            is_icov = is_icovered(samples, 1, 0, d_rev, kci_invariance_test, alpha=alpha)
-
-            if not is_icov:
-                false_negatives += 1
-
-        print("Actual number of false negatives:", false_negatives)
+    # def test_isicovered(self):
+    #     num_trials = 100
+    #     alpha = .05
+    #     num_samples = 100
+    #
+    #     false_negatives = 0
+    #     for i in range(num_trials):
+    #         d = cd.GaussDAG([0, 1], arcs={(0, 1)})
+    #
+    #         samples = {}
+    #         samples[frozenset()] = d.sample(num_samples)
+    #         samples[frozenset({1})] = d.sample_interventional({1: cd.GaussIntervention(2, 1)}, num_samples)
+    #
+    #         d_rev = cd.DAG(arcs={(1, 0)})
+    #         is_icov = is_icovered(samples, 1, 0, d_rev, kci_invariance_test, alpha=alpha)
+    #
+    #         if not is_icov:
+    #             false_negatives += 1
+    #
+    #     print("Actual number of false negatives:", false_negatives)
 
     # def test_3nodes(self):
     #     gdag = cd.GaussDAG([1, 2, 3], arcs={(1, 2), (2, 3)})
