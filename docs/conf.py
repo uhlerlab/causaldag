1fc0bef81d294f52b988f8a24459407ce351ab0a
csquires <chandlersquires18@gmail.com>
2019-02-08 18:24:07 -0500

    mock imports

--- docs/conf.py
+++ docs/conf.py
@@ -46,6 +46,8 @@ extensions = [
 ]
 
 autosummary_generate = True
+autodoc_mock_imports = ['numpy', 'pandas']
+
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
