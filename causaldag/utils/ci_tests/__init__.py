0a392b277b392c919f13498bede5515fffdd1c58
csquires <chandlersquires18@gmail.com>
2019-05-07 14:52:02 -0400

    add gauss_ci_suffstat to exports

--- causaldag/utils/ci_tests/__init__.py
+++ causaldag/utils/ci_tests/__init__.py
@@ -3,6 +3,6 @@ from .invariance_tester import MemoizedInvarianceTester, PlainInvarianceTester, 
 
 from .kci import kci_test_vector, ki_test_vector, kci_test, kci_invariance_test
 from .hsic import hsic_test_vector, hsic_test, hsic_invariance_test
-from .gauss_ci import gauss_ci_test
+from .gauss_ci import gauss_ci_test, gauss_ci_suffstat
 from .oracle import dsep_test, msep_test
 
