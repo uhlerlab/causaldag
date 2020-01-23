62efc2b1cb423e2b06823f00bd5f9fea305058eb
Chandler <csquires@mit.edu>
2020-01-23 16:59:42 -0500

    add export for difference ug

--- causaldag/structure_learning/difference/__init__.py
+++ causaldag/structure_learning/difference/__init__.py
@@ -1,2 +1,2 @@
 from .difference_dag import dci, dci_skeleton, dci_orient
-from .difference_ug import estimate_difference_ug
+from .difference_ug import compute_difference_graph, dci_undirected_graph
