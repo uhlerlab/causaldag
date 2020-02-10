6933ba9483250bf4cf0476f9165618df2d95aef2
Anastasiya <belyaeva@mit.edu>
2020-02-10 18:47:42 -0500

    add stability selection

--- causaldag/structure_learning/difference/difference_dag.py
+++ causaldag/structure_learning/difference/difference_dag.py
@@ -7,8 +7,10 @@ directed acyclic graph (DAG) models given samples from each model.
 
 References
 ----------
-   [1] Wang, Y., Squires, C., Belyaeva, A., & Uhler, C. (2018). Direct estimation of differences in causal graphs. 
-   In Advances in Neural Information Processing Systems (pp. 3770-3781).
+    [1] Wang, Y., Squires, C., Belyaeva, A., & Uhler, C. (2018). Direct estimation of differences in causal graphs. 
+    In Advances in Neural Information Processing Systems (pp. 3770-3781).
+    [2] Meinshausen, N. and Buhlmann, P. (2010). Stability selection.
+    Journal of the Royal Statistical Society: Series B (Statistical Methodology), 72(4), pp.417-473.
 """
 
 
@@ -18,6 +20,10 @@ from causaldag.utils.core_utils import powerset
 from causaldag.utils.regression import RegressionHelper
 from scipy.special import ncfdtr
 from numpy.linalg import inv
+import itertools
+from joblib import Parallel, delayed
+from sklearn.utils import safe_mask
+from sklearn.utils.random import sample_without_replacement
 
 
 def dci(
@@ -74,6 +80,11 @@ def dci(
         could not be determined, are represented by assigning 1 in both directions, i.e. adjacency_matrix[i,j] = 1
         and adjacency_matrix[j,i] = 1. Otherwise for oriented edges, only adjacency_matrix[i,j] = 1 is assigned. 
         Assignment of 0 in the adjacency matrix represents no edge.
+
+    References
+    ----------
+        [1] Wang, Y., Squires, C., Belyaeva, A., & Uhler, C. (2018). Direct estimation of differences in causal graphs. 
+        In Advances in Neural Information Processing Systems (pp. 3770-3781).
     """
 
     assert 0 <= alpha_skeleton <= 1, "alpha_skeleton must be in [0,1] range."
@@ -97,6 +108,141 @@ def dci(
 
     adjacency_matrix = edges2adjacency(num_nodes, edges_unoriented, undirected=True) + edges2adjacency(num_nodes, edges_oriented, undirected=False)
     return adjacency_matrix
+
+
+def dci_stability_selection(
+        X1,
+        X2,
+        alpha_ug_grid: list=[0.1, 1, 10],
+        alpha_skeleton_grid: list=[0.1, 0.5],
+        alpha_orient_grid: list = [0.001, 0.1],
+        max_set_size: int=None,
+        difference_ug: list=None,
+        max_iter: int=1000,
+        edge_threshold: float=0.05,
+        sample_fraction: float=0.7,
+        n_bootstrap_iterations: int=50,
+        bootstrap_threshold: float=0.5,
+        n_jobs: int=1,
+        random_state: int=None,
+        verbose: int=0
+):
+    """
+    Runs Difference Causal Inference (DCI) algorithm with stability selection to estimate the difference-DAG between two settings. 
+    Bootstrap samples are generated from two input datasets and DCI is run across bootstrap samples and across different 
+    combinations of hyperparameters. Edges that reliably appear across different runs are considered stable and are output as the difference-DAG.
+
+    Parameters
+    ----------
+    X1: array, shape = [n_samples, n_features]
+        First dataset.    
+    X2: array, shape = [n_samples, n_features]
+        Second dataset.
+    alpha_ug_grid: array-like, default = [0.1, 1, 10]
+        Grid of values to iterate over representing L1 regularization parameter for estimating the difference undirected graph via KLIEP algorithm.
+    alpha_skeleton: array-like, default = [0.1, 0.5]
+        Grid of values to iterate over representing significance level parameter for determining presence of edges in the skeleton of the difference graph. 
+        Lower alpha_skeleton results in sparser difference graph.
+    alpha_orient: array-like, default = [0.001, 0.1]
+        Grid of values to iterate over representing significance level parameter for determining orientation of an edge. 
+        Lower alpha_orient results in more directed edges in the difference-DAG.
+    max_set_size: int, default = None
+        Maximum conditioning set size used to test regression invariance.
+        Smaller maximum conditioning set size results in faster computation time. For large datasets recommended max_set_size is 3.
+    difference_ug: list, default = None
+        List of tuples that represents edges in the difference undirected graph. If difference_ug is None, 
+        KLIEP algorithm for estimating the difference undirected graph will be run. 
+        If the number of nodes is small, difference_ug could be taken to be the complete graph between all the nodes.
+    max_iter: int, default = 1000
+        Maximum number of iterations for gradient descent in KLIEP algorithm.
+    edge_threshold: float, default = 0.05
+        Edge weight cutoff for keeping an edge for KLIEP algorithm (all edges above or equal to this threshold are kept).
+    sample_fraction: float, default = 0.7
+        The fraction of samples to be used in each bootstrap sample.
+        Should be between 0 and 1. If 1, all samples are used.
+    n_bootstrap_iterations: int, default = 50
+        Number of bootstrap samples to create.
+    bootstrap_threshold: float, default = 0.5
+        Threshold defining the minimum cutoff value for the stability scores. Edges with stability scores above
+        the bootstrap_threshold are kept as part of the difference-DAG.
+    n_jobs: int, default = 1
+        Number of jobs to run in parallel.
+    random_state: int, default = None
+        Seed used by the random number generator.
+    verbose: int, default = 0
+        The verbosity level of logging messages.
+
+    See Also
+    --------
+    dci, dci_undirected_graph, dci_skeleton, dci_orient
+
+    Returns
+    -------
+    adjacency_matrix: array, shape  = [n_features, n_features]
+        Estimated difference-DAG. Edges that were found to be different between two settings but the orientation
+        could not be determined, are represented by assigning 1 in both directions, i.e. adjacency_matrix[i,j] = 1
+        and adjacency_matrix[j,i] = 1. Otherwise for oriented edges, only adjacency_matrix[i,j] = 1 is assigned. 
+        Assignment of 0 in the adjacency matrix represents no edge.
+    stability_scores: array, shape = [n_params, n_features, n_features]
+        Stability score of each edge for for each combination of hyperparameters.
+
+    References
+    ----------
+        [1] Wang, Y., Squires, C., Belyaeva, A., & Uhler, C. (2018). Direct estimation of differences in causal graphs. 
+        In Advances in Neural Information Processing Systems (pp. 3770-3781).
+        [2] Meinshausen, N. and Buhlmann, P. (2010). Stability selection.
+           Journal of the Royal Statistical Society: Series B (Statistical Methodology), 72(4), pp.417-473.
+    """
+
+    _, n_variables = X1.shape
+    n_params = len(alpha_ug_grid)*len(alpha_skeleton_grid)*len(alpha_orient_grid)
+
+    hyperparams = itertools.product(alpha_ug_grid, alpha_skeleton_grid, alpha_orient_grid)        
+    stability_scores = np.zeros((n_params, n_variables, n_variables))
+
+    for idx, params in enumerate(hyperparams):
+        if verbose > 0:
+            print("Fitting estimator for alpha_ug = %.5f, alpha_skeleton = %.5f, alpha_orient = %.5f with %d bootstrap iterations" %
+                  (params[0], params[1], params[2], n_bootstrap_iterations))
+
+        bootstrap_samples1 = bootstrap_generator(n_bootstrap_iterations, sample_fraction,
+                                                 X1, random_state=random_state)
+        bootstrap_samples2 = bootstrap_generator(n_bootstrap_iterations, sample_fraction,
+                                                 X2, random_state=random_state)
+
+
+        bootstrap_results = Parallel(n_jobs, verbose=verbose
+        )(delayed(dci)(X1[safe_mask(X1, subsample1), :],
+            X2[safe_mask(X2, subsample2), :],
+            alpha_ug = params[0],
+            alpha_skeleton = params[1],
+            alpha_orient = params[2],
+            max_set_size = max_set_size,
+            difference_ug = difference_ug,
+            max_iter = max_iter,
+            edge_threshold = edge_threshold,
+            verbose = verbose)
+          for subsample1, subsample2 in zip(bootstrap_samples1, bootstrap_samples2))
+
+
+        stability_scores[idx] = np.array(bootstrap_results).mean(axis=0)
+
+    adjacency_matrix = choose_stable_variables(stability_scores, bootstrap_threshold=bootstrap_threshold)
+    return adjacency_matrix, stability_scores
+
+
+def choose_stable_variables(stability_scores, bootstrap_threshold=0.5):
+    """Returns adjacency matrix corresponding to edges with stability scores above threshold."""
+    return (stability_scores.max(axis=0) > bootstrap_threshold).astype('float')
+
+
+def bootstrap_generator(n_bootstrap_iterations, sample_fraction, X, random_state=None):
+    """Generates bootstrap samples from dataset."""
+    n_samples = len(X)
+    n_subsamples = np.floor(sample_fraction * n_samples).astype(int)
+    for _ in range(n_bootstrap_iterations):
+        subsample = sample_without_replacement(n_samples, n_subsamples, random_state=random_state)
+        yield subsample
 
 
 def dci_skeleton(
