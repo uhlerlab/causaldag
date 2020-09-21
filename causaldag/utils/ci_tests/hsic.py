867721de25d3ba7fdb4ff4ea58b73efd417409e0
Chandler Squires <csquires@mit.edu>
2020-09-21 16:22:07 -0400

    make hsic kernels faster

--- causaldag/utils/ci_tests/hsic.py
+++ causaldag/utils/ci_tests/hsic.py
@@ -4,6 +4,8 @@ from causaldag.utils.ci_tests import kernels
 from scipy.stats import gamma
 from causaldag.utils.ci_tests._utils import residuals
 from causaldag.utils.core_utils import to_list
+from scipy.special import gdtr
+import ipdb
 
 
 def hsic_test_vector(
@@ -39,14 +41,14 @@ def hsic_test_vector(
         raise ValueError("Y should have the same number of samples as X")
 
     n = x.shape[0]
-    H = np.eye(n) - np.ones([n, n])/n
     kernel_precision = 1/(sig**2)
 
     # === COMPUTE CENTRALIZED KERNEL MATRICES
-    kx = kernels.rbf_kernel(x, kernel_precision)
-    ky = kernels.rbf_kernel(y, kernel_precision)
+    kx = kernels.rbf_kernel_fast(x, kernel_precision)
+    ky = kernels.rbf_kernel_fast(y, kernel_precision)
     kx_centered = kernels.center(kx)
     ky_centered = kernels.center(ky)
+    # ipdb.set_trace()
 
     # === COMPUTE STATISTIC
     statistic = 1/n**2 * np.sum(kx_centered * ky_centered.T)  # SAME AS trace(kx_centered @ ky_centered)
@@ -56,17 +58,20 @@ def hsic_test_vector(
     mean_approx = 1/n * (1 + mu_x*mu_y - mu_x - mu_y)
     var_approx = 2*(n-4)*(n-5)/(n*(n-1)*(n-2)*(n-3)) * np.sum(kx_centered * kx_centered.T) * np.sum(ky_centered * ky_centered.T) / n**4
 
+    # NEW
     k_approx = mean_approx ** 2 / var_approx
     prec_approx = var_approx / mean_approx
 
-    critval = gamma.ppf(1-alpha, k_approx, scale=prec_approx)
-    p_value = 1 - gamma.cdf(statistic, k_approx, scale=prec_approx)
+    # critval = gamma.ppf(1-alpha, k_approx, scale=prec_approx)
+    # p_value = 1 - gamma.cdf(statistic, k_approx, scale=prec_approx)
+    p_value = 1 - gdtr(1/prec_approx, k_approx, statistic)
+    # print(p_value, p_value_)
 
     return dict(
         statistic=statistic,
-        critval=critval,
+        # critval=critval,
         p_value=p_value,
-        reject=statistic > critval,
+        reject=p_value < alpha,
         mean_approx=mean_approx,
         var_approx=var_approx
     )
@@ -108,7 +113,16 @@ def hsic_test(
         return hsic_test_vector(residuals_i, residuals_j, alpha=alpha)
 
 
+if __name__ == '__main__':
+    import numpy as np
+    from line_profiler import LineProfiler
 
-
+    lp = LineProfiler()
+    X1 = np.random.laplace(0, 1, size=1000)
+    X2 = np.random.laplace(0, 1, size=1000)
+    lp.add_function(hsic_test_vector)
+    for _ in range(10):
+        lp.runcall(hsic_test_vector, X1, X2)
+    lp.print_stats()
 
 
